f3c570f48fd5bcc60a8af59bc4a833821d74d84e
Roman Stetsenko <Roman_Stetsenko@mail.com>
2018-08-09 12:13:46 +0200

    remove extra type

--- AnotherTest/src/ConsoleApp1/Services/MarketValueService.cs
+++ AnotherTest/src/ConsoleApp1/Services/MarketValueService.cs
@@ -15,35 +15,14 @@ namespace ConsoleApp1.Services
             {
                 var marketValues = positions
                     .Join(prices,
-                        position => new PricePositionJunction(position),
-                        price => new PricePositionJunction(price),
+                        position => (position.ProductKey, position.Date),
+                        price => (price.ProductKey, price.Date),
                         (position, price) => new MarketValue(position, price));
                 return new Success<IEnumerable<MarketValue>>(marketValues);
             }
             catch (Exception e)
             {
                 return new Failure<IEnumerable<MarketValue>>(e.Message);
-            }
-        }
-
-        internal struct PricePositionJunction
-        {
-            public PricePositionJunction(Position position)
-            {
-                if (position == null) throw new ArgumentNullException(nameof(position));
-                Date = position.Date;
-                ProductKey = position.ProductKey;
-            }
-
-            public string ProductKey { get; }
-
-            public DateTime Date { get; }
-
-            public PricePositionJunction(Price price)
-            {
-                if (price == null) throw new ArgumentNullException(nameof(price));
-                Date = price.Date;
-                ProductKey = price.ProductKey;
             }
         }
     }
