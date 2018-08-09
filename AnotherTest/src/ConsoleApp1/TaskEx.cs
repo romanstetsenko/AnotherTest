15f3547c12e950711c5709e37949204021c02749
Roman Stetsenko <Roman_Stetsenko@mail.com>
2018-08-09 10:02:21 +0200

    remove extra method

--- AnotherTest/src/ConsoleApp1/TaskEx.cs
+++ AnotherTest/src/ConsoleApp1/TaskEx.cs
@@ -9,10 +9,5 @@ namespace ConsoleApp1
             await Task.WhenAll(task1, task2);
             return (task1.Result, task2.Result);
         }
-        public static async Task<(T1, T2)> WaitAll<T1, T2>(Task<T1> task1, Task<T2> task2)
-        {
-            await Task.WhenAll(task1, task2);
-            return (task1.Result, task2.Result);
-        }
     }
 }
